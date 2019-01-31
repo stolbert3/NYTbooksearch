e4bcade39ce7839bd854eb215bd5484444f450c5
Sydney <stolbert3@gatech.edu>
2019-01-31 16:38:38 -0500

    fixing pages

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
 import Jumbotron from "../../components/Jumbotron";
+import ViewBtn from "../../components/ViewBtn";
 import DeleteBtn from "../../components/DeleteBtn";
 
 class Saved extends Component {
-  
+
 };
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
 import Jumbotron from "../../components/Jumbotron";
 import ViewBtn from "../../components/ViewBtn";
 import SaveBtn from "../../components/SaveBtn";
